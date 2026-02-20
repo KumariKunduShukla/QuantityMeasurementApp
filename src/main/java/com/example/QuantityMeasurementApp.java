842434f1af92874ebfd7845b289efa2d6f17d61b
KumariKunduShukla <kundushukla55@gmail.com>
2026-02-20 17:21:26 +0530

    [Kumari] : completed UC3 task

--- src/main/java/com/example/QuantityMeasurementApp.java
+++ src/main/java/com/example/QuantityMeasurementApp.java
@@ -1,78 +1,24 @@
 package com.example;
 
-import java.util.Scanner;
-
 public class QuantityMeasurementApp {
 
-	public static class Feet {
+	public static void demonstrateFeetEquality() {
+		Quantity f1 = new Quantity(1.0, Unit.FEET);
+		Quantity f2 = new Quantity(1.0, Unit.FEET);
 
-		private final double value;
-
-		public Feet(double value) {
-			this.value = value;
-		}
-
-		public double getValue() {
-			return value;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj) {
-				return true;
-			}
-
-			if (obj == null || getClass() != obj.getClass()) {
-				return false;
-			}
-
-			Feet other = (Feet) obj;
-			return Double.compare(this.value, other.value) == 0;
-		}
-
-		@Override
-		public int hashCode() {
-			return Double.hashCode(value);
-		}
+		System.out.println("Feet Equal: " + f1.equals(f2));
 	}
 
-	// Inner class to represent Inch measurement
-		public static class Inches {
-			private final double value;
+	public static void demonstrateInchesEquality() {
+		Quantity i1 = new Quantity(1.0, Unit.INCH);
+		Quantity i2 = new Quantity(1.0, Unit.INCH);
 
-			public Inches(double value) {
-				this.value = value;
-			}
-
-			@Override
-			public boolean equals(Object obj) {
-				if (this == obj)
-					return true;
-
-				if (obj == null || getClass() != obj.getClass())
-					return false;
-
-				Inches inches = (Inches) obj;
-
-				return Double.compare(this.value, inches.value) == 0;
-			}
-		}
-
-		public static void demonstrateFeetEquality() {
-			Feet f1 = new Feet(1.0);
-			Feet f2 = new Feet(1.0);
-			System.out.println("Feet Equal: " + f1.equals(f2));
-		}
-
-		public static void demonstrateInchesEquality() {
-			Inches i1 = new Inches(1.0);
-			Inches i2 = new Inches(1.0);
-			System.out.println("Inches Equal: " + i1.equals(i2));
-		}
-
-		public static void main(String[] args) {
-			demonstrateFeetEquality();
-			demonstrateInchesEquality();
-		}
-
+		System.out.println("Inches Equal: " + i1.equals(i2));
 	}
+
+	public static void main(String[] args) {
+		demonstrateFeetEquality();
+		demonstrateInchesEquality();
+	}
+
+}
---
+++ src/main/java/com/example/Unit.java
@@ -0,0 +1,6 @@
+package com.example;
+
+public enum Unit {
+    FEET,
+    INCH
+}
