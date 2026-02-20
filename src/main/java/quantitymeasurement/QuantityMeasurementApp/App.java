15e3aac3566779e122e12d8030b1d2f46e04c475
KumariKunduShukla <kundushukla55@gmail.com>
2026-02-20 16:46:34 +0530

    [Kumari] : added initial setup

---
+++ src/main/java/quantitymeasurement/QuantityMeasurementApp/App.java
@@ -0,0 +1,10 @@
+package quantitymeasurement.QuantityMeasurementApp;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
